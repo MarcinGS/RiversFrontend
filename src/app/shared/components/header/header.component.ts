a7d7d707a9294efd7d01fe5b82bb85a933c2c97d
Marcin <golismarcin@gmail.com>
2023-01-22 21:49:41 +0100

    angular material stylowanie

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  title = "Rivers Level Monitor";
   constructor() { }
 
   ngOnInit(): void {
